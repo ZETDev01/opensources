66728947616b9a1c373c071f369bdaba4039e732
ZETDev01 <trihuy2001y@gmail.com>
2023-11-08 07:53:07 +0700

    Giải n phương trình n ẩn

---
+++ Thuchanh_1/exercise1.py
@@ -0,0 +1,16 @@
+#1-	Thiết kế phần mềm ứng dụng giải hệ phương trình tuyến tính n phương trình n ẩn
+import numpy as np
+n=int(input('Nhập số phương trình: '))
+m=int(input('Nhập số ẩn: '))
+X=np.zeros((n,m))
+x=np.zeros(n)
+print("Nhập ma trận hệ số X: ")
+for i in range(n):
+  for j in range(m):
+    X[i][j]=float(input(f"A[{i+1},{j+1}]: "))
+print("Nhập vector x: ")
+for i in range(n):
+  x[i]=float(input(f"b[{j+1}]: "))
+kq=np.linalg.solve(X,x)
+print("Nghiệm phương trình là: ",kq)
+
